147eec0b63027c364ec0e9a913e2046cf4ba0d10
Toni Lamont <tlam212@gmail.com>
2018-06-19 15:01:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(attendees)
+  "Hello, my name is #{attendees}."
+end
+
+def batch_badge_creator(attendees)
+  badges = badge_maker(attendees)
+  badges.each do 
+
+ 
+end
+
+  
+  
+
+
+
+  
+  
